59e0a8b7d9600ed21ea057ac52af8eaae7edc696
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2025-04-20 00:58:53 +0400

    MainWindow

---
+++ Views/A4PagePreview.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MarkdownEditor;
+using System.Windows.Controls;
+
+public partial class A4PagePreview : UserControl
+{
+    public A4PagePreview()
+    {
+        InitializeComponent();
+        DataContextChanged += (s, e) =>
+        {
+            if (e.NewValue is MainWindow vm)
+                PreviewBrowser.NavigateToString(vm.HtmlPreview);
+        };
+    }
+}
