fa62165a1dae5a95e5f9d292b957fd370fc99965
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2025-04-19 23:53:27 +0400

    MainViewModel

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Markdig;
+using MarkdownEditor.Models;
+using System.ComponentModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private string _markdownText = "";
+    public string MarkdownText
+    {
+        get => _markdownText;
+        set
+        {
+            _markdownText = value;
+            OnPropertyChanged(nameof(MarkdownText));
+            OnPropertyChanged(nameof(HtmlPreview));
+
+        }
+    }
+
+    public string HtmlPreview => Markdig.Markdown.ToHtml(MarkdownText);
+
+    public PageSettings PageSettings { get; set; } = new PageSettings();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
