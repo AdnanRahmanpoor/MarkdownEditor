8837a38b13370f7da0ce6a80c772b4bf75f84e5d
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2025-04-20 01:05:56 +0400

    PDF Export service

---
+++ Services/PdfExportService.cs
@@ -0,0 +1,30 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using Markdig;
+using MarkdownEditor.Models;
+
+public static class PdfExportService
+{
+    public static void ExportToPdf(string markdown, string filePath, PageSettings settings)
+    {
+        var document = new PdfDocument();
+        var page = document.AddPage();
+        page.Width = settings.Width;
+        page.Height = settings.Height;
+
+        using (XGraphics gfx = XGraphics.FromPdfPage(page))
+        {
+            XFont font = new XFont("Arial", 12);
+            var text = Markdown.ToPlainText(markdown);
+            XRect layout = new XRect(
+                settings.Margins.Left,
+                settings.Margins.Top,
+                page.Width - settings.Margins.Left - settings.Margins.Right,
+                page.Height - settings.Margins.Top - settings.Margins.Bottom
+                );
+            gfx.DrawString(text, font, XBrushes.Black, layout);
+        }
+
+        document.Save(filePath);
+    }
+}
