fe761a1691ca1c2b9a76a5db582650487c206ba8
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2025-04-19 23:39:51 +0400

    A4 PageSetting and Setup

---
+++ Models/PageSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MarkdownEditor.Models
+{
+    public class PageSettings
+    {
+        public double Width { get; set; } = 794; // A4 page width
+        public double Height { get; set; } = 1123; // A4 page height
+        public Thickness Margins { get; set; } = new Thickness(50); // Default 50px margins
+    }
+}
